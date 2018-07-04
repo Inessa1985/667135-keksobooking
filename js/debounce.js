5d452f2a447353153427174c12a453be50077ecc
Inessa <panda1756@yandex.ru>
2018-07-04 12:36:42 +0300

    Удаляет комменты в window.debounce

--- js/debounce.js
+++ js/debounce.js
@@ -3,21 +3,6 @@
 (function () {
 
   var DEBOUNCE_INTERVAL = 500; // ms
-  // var lastTimeout = null;
-
-  /*
-  window.debounce = function (fun) {
-
-    return function () {
-      var args = arguments;
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = window.setTimeout(function () {
-        fun.apply(null, args);
-      }, DEBOUNCE_INTERVAL);
-    };
-  };*/
 
   window.debounce = function (action) {
     var lastTimeout;
